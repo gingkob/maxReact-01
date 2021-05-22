003d1c5c2c2b9d58bf248a031767f0e60340cb88
gingkob <malivlada@hotmail.com>
2021-05-22 22:04:31 +0200

    modal moved into portals

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
+const modalEl = document.getElementById('modal');
+
 const Modal = (props) => {
-  return (
+  return ReactDOM.createPortal(
     <div className={classes['modal-container']} onClick={props.onClose}>
       {props.children}
-    </div>
+    </div>,
+    modalEl
   )
 }
 
